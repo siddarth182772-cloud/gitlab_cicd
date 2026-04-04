cfede42866393a36a59a3e4e08d94b3fe6ac4d38
siddarth182772-cloud <siddarth.182772@gmail.com>
2026-04-04 10:03:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">4.0" #morthan 4.0 version
+    }
+  }
+}
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "sid_vpc"
+    }
+  
+}
+resource "aws_subnet" "name2" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+      Name = "sid-subnet"
+    }
+  
+}
